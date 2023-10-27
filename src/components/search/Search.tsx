d3dc166ded74963775dd1860a64ba9a150d20c2d
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-10-27 15:07:42 +0500

    feat: add Search component

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,17 +1,47 @@
 import { Component } from 'react';
+import styles from './Search.module.scss';
 
-export class Search extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+interface SearchState {
+  input: string;
+}
+
+export class Search extends Component<object, SearchState> {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      input: localStorage.getItem('searchInput') || '',
+    };
+  }
+
+  onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      input: event.currentTarget.value,
+    });
+  };
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    localStorage.setItem('searchInput', this.state.input);
+  };
 
   render() {
     return (
       <div>
-        <label htmlFor="search">
-          <input type="search" name="search" />
-        </label>
-        Search
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="search">
+            <input
+              className={styles.search}
+              type="search"
+              name="search"
+              id="search"
+              placeholder="You can search here"
+              onChange={this.onChange}
+              value={this.state.input}
+            />
+          </label>
+          <input type="submit" value="Search" />
+        </form>
+        <p>{this.state.input}</p>
       </div>
     );
   }
